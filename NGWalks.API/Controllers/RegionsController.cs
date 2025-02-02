94c2d28793fb3ab98590095d255e044450690789
Uthman007 <adewuyiuthmanalao@gmail.com>
2025-02-02 23:27:26 +0100

    Initial commit after adding DTO class

---
+++ NGWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NGWalks.API.Data;
+using NGWalks.API.Models.Domain;
+using NGWalks.API.Models.DTO;
+
+namespace NGWalks.API.Controllers
+{
+    //https://localhost:7195/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NGWalksDbContext dbContext;
+
+        public RegionsController(NGWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        //Get All regions
+        //GET: https://localhost:7195/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Get Data from database - Domain models
+            var regionsDomain = dbContext.Regions.ToList();
+
+            //Map Domain Models to DTOs
+            var regionsDto = new List<RegionDto>();
+            foreach (var regionDomain in regionsDomain) 
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = regionDomain.Id,
+                    Name = regionDomain.Name,
+                    Code = regionDomain.Code,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+                });
+            
+            }
+
+            //Return DTOs
+            return Ok(regionsDomain);
+        }
+
+
+        //Get single region (get region by id)
+        //GET: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //var region = dbContext.Regions.Find(id);   //a way to get id colume from the database.
+
+            // another way to get a single colume in region from the database
+            var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regionDomain == null)
+            { 
+                return NotFound();
+            }
+
+            //Map region domain model to region Dto
+            //
+
+            var regionDto = new RegionDto
+            {
+                Id = regionDomain.Id,
+                Name = regionDomain.Name,
+                Code = regionDomain.Code,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+
+
+            //Return DTO back to client
+            return Ok(regionDto);                                                                                                                                                      
+        }
+    }
+}
---
+++ NGWalks.API/Data/NGWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NGWalks.API.Models.Domain;
+
+namespace NGWalks.API.Data
+{
+    public class NGWalksDbContext: DbContext
+    {
+        public NGWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
